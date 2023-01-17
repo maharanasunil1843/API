484cc217f841de0296a9faab622c53956a25d134
Sunil <maharanasunil1843@gmail.com>
2023-01-17 09:41:27 +0530

    API Testing

--- test.py
+++ test.py
@@ -22,4 +22,9 @@ def test3():
 if __name__ == '__main__': #this invokes the entire python class
     app.run()
 
-
+Task:
+1. Write a program to insert a record in sql table via api database
+2. Write a program to update a record via api
+3. Write a program to delete a record via api
+4. Write a program to detch a record via api
+5. All the abov questions you have to anser for mongo db as well.
